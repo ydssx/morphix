359a085170a77c897d836a912db6e4e703d113aa
ydssx <wangxin@91cyt.com>
2023-07-23 19:42:33 +0800

    1

--- app/gateway/cmd/gateway/handlers.go
+++ app/gateway/cmd/gateway/handlers.go
@@ -1,25 +1,9 @@
 package main
 
 import (
-	"context"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/health/grpc_health_v1"
-	"google.golang.org/grpc/status"
+	"github.com/gin-gonic/gin"
 )
 
-type ServerInfo struct {
-	Name   string
-	Status string
-	Msg    string
-}
-
-func healthzServer(conn *grpc.ClientConn) (r ServerInfo) {
-	cli := grpc_health_v1.NewHealthClient(conn)
-	res, err := cli.Check(context.TODO(), &grpc_health_v1.HealthCheckRequest{Service: ""})
-	if err != nil {
-		r.Msg = status.Convert(err).Message()
-	}
-	r.Status = res.GetStatus().String()
-	return
+func healthzServer(c *gin.Context) {
+	c.String(200, "%s", "ok")
 }
