3228c52c7f4dfb1c5deb13f3ff35c503423f90c9
wx <wx>
2023-08-16 18:06:18 +0800

    1

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 set -eo pipefail
 
+
 # 阿里云容器镜像服务相关信息
 REGISTRY_URL="registry.cn-shenzhen.aliyuncs.com"
 REGISTRY_NAMESPACE="ydssx"
@@ -14,20 +15,54 @@ version="1.0.3"
 # 登录镜像仓库
 echo "$registry_password" | docker login --username "$registry_username" --password-stdin $REGISTRY_URL
 
-
-# 查找微服务目录下的 Dockerfile 文件，并迭代构建和推送镜像
+# 定义要构建的微服务列表
 services=("gateway" "user" "sms" "order" "payment")
-for service in "${services[@]}"; do
+
+# 构建和推送指定的微服务镜像
+build_and_push() {
+    service="$1"
     image_name="$REGISTRY_URL/$REGISTRY_NAMESPACE/$service:$version"
-    
+
     echo "构建镜像：$image_name"
-    
+
     # 构建镜像
     TAG="$version" docker-compose build "$service"
-        
+
     # 推送镜像到阿里云容器镜像服务
     docker push "$image_name"
-    
+
     echo "镜像构建和推送完成：$image_name"
     echo
-done
+}
+
+# 最大并发数
+max_concurrency=3
+current_jobs=0
+
+# 如果传入了服务名参数，则只构建和推送指定的服务
+if [ $# -gt 0 ]; then
+    for service in "$@"; do
+        build_and_push "$service" &
+        ((current_jobs++))
+        
+        if [ $current_jobs -ge $max_concurrency ]; then
+            wait
+            current_jobs=0
+        fi
+    done
+else
+    # 否则，构建和推送所有微服务镜像
+    services=("gateway" "user" "sms" "order" "payment")
+    for service in "${services[@]}"; do
+        build_and_push "$service" &
+        ((current_jobs++))
+        
+        if [ $current_jobs -ge $max_concurrency ]; then
+            wait
+            current_jobs=0
+        fi
+    done
+fi
+
+# 确保最后一批任务完成
+wait
