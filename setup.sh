0e4116a616249c83512c9374f7d95ed99656922b
wx <wx>
2023-09-22 17:57:03 +0800

    1

--- setup.sh
+++ setup.sh
@@ -8,3 +8,7 @@ helm upgrade --install phpmyadmin bitnami/phpmyadmin -f deploy/kubernetes/helm/p
 helm upgrade --install prometheus prometheus-community/prometheus -f deploy/kubernetes/helm/prometheus.yaml
 helm upgrade --install loki --namespace=loki-stack grafana/loki-stack -f deploy/kubernetes/helm/loki.yaml
 helm upgrade --install otelcol opentelemetry-helm/opentelemetry-collector -f deploy/kubernetes/helm/otelcol.yaml
+helm upgrade --install thanos bitnami/thanos -f deploy/kubernetes/helm/thanos.yaml
+helm upgrade --install jaeger jaegertracing/jaeger -f deploy/kubernetes/helm/jaeger.yaml
+helm upgrade --install argocd argo/argo-cd -f deploy/kubernetes/helm/argocd.yaml --namespace=argocd --create-namespace
+helm upgrade --install sealed-secrets -n kube-system --set-string fullnameOverride=sealed-secrets-controller sealed-secrets/sealed-secrets
