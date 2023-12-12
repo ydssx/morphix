5a7f5b68fc977faf2a13e833ceebd491984a962b
ydssx <wx>
2023-12-12 09:30:54 +0800

    Upgrade Go version and refactor Aiart biz logic
    Upgrade Go version in Dockerfile from 1.19.2 to 1.21.4
    Refactor Aiart biz logic by adding Transaction interface and renaming AiartUseCase methods

--- app/aiart/internal/biz/aiart.go
+++ app/aiart/internal/biz/aiart.go
@@ -6,16 +6,22 @@ import (
 	aiartv1 "github.com/ydssx/morphix/api/aiart/v1"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
-
-type AiartUseCase struct {
+// Transaction is a transaction interface.
+type Transaction interface {
+	// InTx executes f in a transaction.
+	// If f returns a non-nil error, the transaction will be rolled back.
+	// If f returns nil, the transaction will be committed.
+	InTx(context.Context, func(ctx context.Context) error) error
 }
+
+type AiartUseCase struct{}
 
 func NewAiartUseCase() *AiartUseCase {
 	return &AiartUseCase{}
 }
 
 // 生成图像
-func (b *AiartUseCase) GenerateImage(ctx context.Context, req *aiartv1.GenerateImageRequest) (res *aiartv1.GenerateImageResponse, err error) {
+func (uc *AiartUseCase) GenerateImage(ctx context.Context, req *aiartv1.GenerateImageRequest) (res *aiartv1.GenerateImageResponse, err error) {
 	res = new(aiartv1.GenerateImageResponse)
 
 	// TODO:ADD logic here and delete this line.
@@ -24,7 +30,7 @@ func (b *AiartUseCase) GenerateImage(ctx context.Context, req *aiartv1.GenerateI
 }
 
 // 获取生成任务状态
-func (b *AiartUseCase) GetGenerateStatus(ctx context.Context, req *aiartv1.GetGenerateStatusRequest) (res *aiartv1.GenerateStatusResponse, err error) {
+func (uc *AiartUseCase) GetGenerateStatus(ctx context.Context, req *aiartv1.GetGenerateStatusRequest) (res *aiartv1.GenerateStatusResponse, err error) {
 	res = new(aiartv1.GenerateStatusResponse)
 
 	// TODO:ADD logic here and delete this line.
@@ -33,7 +39,7 @@ func (b *AiartUseCase) GetGenerateStatus(ctx context.Context, req *aiartv1.GetGe
 }
 
 // 获取已生成的图像
-func (b *AiartUseCase) GetGeneratedImage(ctx context.Context, req *aiartv1.GetGeneratedImageRequest) (res *aiartv1.GetGeneratedImageResponse, err error) {
+func (uc *AiartUseCase) GetGeneratedImage(ctx context.Context, req *aiartv1.GetGeneratedImageRequest) (res *aiartv1.GetGeneratedImageResponse, err error) {
 	res = new(aiartv1.GetGeneratedImageResponse)
 
 	// TODO:ADD logic here and delete this line.
@@ -42,7 +48,7 @@ func (b *AiartUseCase) GetGeneratedImage(ctx context.Context, req *aiartv1.GetGe
 }
 
 // 获取模型信息
-func (b *AiartUseCase) GetModelInfo(ctx context.Context, req *emptypb.Empty) (res *aiartv1.GetModelInfoResponse, err error) {
+func (uc *AiartUseCase) GetModelInfo(ctx context.Context, req *emptypb.Empty) (res *aiartv1.GetModelInfoResponse, err error) {
 	res = new(aiartv1.GetModelInfoResponse)
 
 	// TODO:ADD logic here and delete this line.
