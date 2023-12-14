a817c3697346bfe48f8938415ad8f87e270feb0c
ydssx <wx>
2023-12-14 11:05:17 +0800

    1

--- common/rbac/rbac.go
+++ common/rbac/rbac.go
@@ -15,12 +15,15 @@ var (
 	once     sync.Once
 )
 
+// NewCasbinEnforcer 创建并返回一个新的 CasbinEnforcer 实例。
 func NewCasbinEnforcer() *casbin.Enforcer {
 	once.Do(func() {
+		// 创建 GORM 的适配器。
 		a, err := gormadapter.NewAdapter("mysql", "root:root@tcp(127.0.0.1:3306)/")
 		if err != nil {
 			panic(err)
 		}
+		// 创建 Casbin 的 Enforcer 实例。
 		e, err := casbin.NewEnforcer("rbac_model.conf", a)
 		if err != nil {
 			panic(err)
@@ -32,19 +35,23 @@ func NewCasbinEnforcer() *casbin.Enforcer {
 	return enforcer
 }
 
+// RbacManager 是 RBAC 管理器的结构体。
 type RbacManager struct {
 	enforcer *casbin.Enforcer
 }
 
+// NewRbacManager 创建并返回一个新的 RbacManager 实例。
 func NewRbacManager(dsn string) *RbacManager {
 	return &RbacManager{enforcer: NewCasbinEnforcer()}
 }
 
+// AddUserRoles 为用户添加角色。
 func (r *RbacManager) AddUserRoles(userID int, roles ...string) error {
 	_, err := r.enforcer.AddRolesForUser(strconv.Itoa(userID), roles)
 	return err
 }
 
+// DeleteUserRoles 删除用户的角色。
 func (r *RbacManager) DeleteUserRoles(userID int, roles ...string) error {
 	var errs []error
 
@@ -56,6 +63,7 @@ func (r *RbacManager) DeleteUserRoles(userID int, roles ...string) error {
 	return errors.Join(errs...)
 }
 
+// DeleteRoles 删除角色。
 func (r *RbacManager) DeleteRoles(roles ...string) error {
 	var errs []error
 
@@ -67,8 +75,18 @@ func (r *RbacManager) DeleteRoles(roles ...string) error {
 	return errors.Join(errs...)
 }
 
+// CheckPermission checks if the given user has permission to access the specified path and method.
+// It first checks if the user has direct permission.
+// If not, it retrieves the roles for the user and checks if any role has permission.
+// If no permission is found, an error is returned indicating insufficient permissions.
 func (r *RbacManager) CheckPermission(userID int, path, meth string) error {
-	userRoles, err := r.enforcer.GetRolesForUser(strconv.Itoa(userID))
+	x := strconv.Itoa(userID)
+	ok, _ := r.enforcer.Enforce(x, path, meth)
+	if ok {
+		return nil
+	}
+
+	userRoles, err := r.enforcer.GetRolesForUser(x)
 	if err != nil {
 		return err
 	}
@@ -90,7 +108,31 @@ func (r *RbacManager) CheckPermission(userID int, path, meth string) error {
 	return errors.New("权限不足")
 }
 
+// AddRolePermission adds a permission rule to the specified role.
+// It takes in the role name, resource path, and access method as strings.
+// It calls the enforcer's AddPolicy method to add the permission rule.
+// Returns any error from the enforcer.
 func (r *RbacManager) AddRolePermission(role, path, meth string) error {
 	_, err := r.enforcer.AddPolicy(role, path, meth)
+	return err
+}
+
+// AddUserPermission adds a permission rule for the given user ID.
+// It takes the user ID as an integer, plus the resource path and access method as strings.
+// It calls the enforcer's AddPolicy method to add the permission rule for that user.
+// Returns any error from the enforcer.
+func (r *RbacManager) AddUserPermission(userID int, path, meth string) error {
+	_, err := r.enforcer.AddPolicy(strconv.Itoa(userID), path, meth)
+	return err
+}
+
+// DeleteRolePermission 删除角色的权限。
+func (r *RbacManager) DeleteRolePermission(role, path, meth string) error {
+	_, err := r.enforcer.RemovePolicy(role, path, meth)
+	return err
+}
+
+func (r *RbacManager) DeleteUserPermission(userID int, path, meth string) error {
+	_, err := r.enforcer.RemovePolicy(strconv.Itoa(userID), path, meth)
 	return err
 }
