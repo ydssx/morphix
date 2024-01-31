5eedceedb94d25859fadb35da9e2b0fc95b8c57b
ydssx <wx>
2024-01-31 11:30:55 +0800

    1

---
+++ scripts/locustfile.py
@@ -0,0 +1,7 @@
+from locust import HttpUser, task,between
+
+class HelloWorldUser(HttpUser):
+    wait_time = between(1, 5)
+    @task
+    def hello_world(self):
+        self.client.get("/v1/images/generate_status")
